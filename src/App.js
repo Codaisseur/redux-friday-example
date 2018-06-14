dcaeec151736def4e8feedc35dec995f9c451887
Bram Koot <git@bramkoot.nl>
2018-06-14 21:37:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Provider} from 'react-redux'
+import store from './store'
+import AlbumsListContainer from './components/AlbumsListContainer'
+import { Route } from 'react-router-dom'
+import PhotoPageContainer from './components/PhotoPageContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Route exact path="/" component={AlbumsListContainer} />
+          <Route exact path="/albums/:id" component={PhotoPageContainer} />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
